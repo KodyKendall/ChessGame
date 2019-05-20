41731af810e6811f92fe9b681196c7147ef1f7dc
KodyKendall <kodyckendall@gmail.com>
2019-05-19 21:00:08 -0600

    Added in logic for initializing the game pieces for the model

--- model/chess/GameBoard.java
+++ model/chess/GameBoard.java
@@ -1,6 +1,7 @@
 package model.chess;
 
 import java.awt.Point;
+import java.util.ArrayList;
 
 /**
  * Represents a chess gameboard and the chess pieces.
@@ -17,10 +18,12 @@ public class GameBoard {
 	 * [8,8] -> [1, h]
 	 */
 	private int[][] board = new int[8][8];
-	private GamePiece[] whiteGamePieces;
-	private GamePiece[] blackGamePieces;
+	private ArrayList<GamePiece> whiteGamePieces;
+	private ArrayList<GamePiece> blackGamePieces;
 	
 	public GameBoard() {
+		this.whiteGamePieces = new ArrayList<GamePiece>();
+		this.blackGamePieces = new ArrayList<GamePiece>();
 		this.initializeGameBoardPieces();
 	}
 	
@@ -29,20 +32,87 @@ public class GameBoard {
 	 */
 	private void initializeGameBoardPieces() {
 		//Need to create eight pawns for white and black.
-		initializePawnsOnRow(1);
+		
+		//initialize all pawns first. 
+		this.blackGamePieces.addAll(this.initializePawnsOnRow(1));
+		this.whiteGamePieces.addAll(this.initializePawnsOnRow(14));
+	
+		//initialize rooks
+		this.blackGamePieces.addAll(this.initializeRooksOnRow(0));
+		this.whiteGamePieces.addAll(this.initializeRooksOnRow(15));
+		
+		//initialize horses
+		this.blackGamePieces.addAll(this.initializeHorsesOnRow(0));
+		this.whiteGamePieces.addAll(this.initializeHorsesOnRow(15));
+		
+		//initialize bishops
+		this.blackGamePieces.addAll(this.initializeBishopsOnRow(0));
+		this.whiteGamePieces.addAll(this.initializeBishopsOnRow(15));
+		
+		//initialize queens
+		this.blackGamePieces.addAll(this.initializeQueenOnRow(0));
+		this.whiteGamePieces.addAll(this.initializeQueenOnRow(15));
+		
+		//initialize kings
+		this.blackGamePieces.addAll(this.initializeKingOnRow(0));
+		this.whiteGamePieces.addAll(this.initializeKingOnRow(15));
 	}
 	
 	/**
 	 * Initializes 8 pawns on the given row
 	 * @param rowCoord
 	 */
-	private GamePiece[] initializePawnsOnRow(int rowCoord) {
-		GamePiece[] pawnsThatWereInitialized = new GamePiece[8];
-		int pawnToAddIndex = 0;
+	private ArrayList<Pawn> initializePawnsOnRow(int rowCoord) {
+		ArrayList<Pawn> pawnsThatWereInitialized = new ArrayList<Pawn>();		
 		for (int colIdx = 0; colIdx < 8; colIdx++) {
 			Pawn pawnToAdd = new Pawn(new Point(rowCoord, colIdx));
-			pawnsThatWereInitialized[pawnToAddIndex] = pawnToAdd;
+			pawnsThatWereInitialized.add(pawnToAdd);
 		}
 		return pawnsThatWereInitialized;
 	}
+	
+	/**
+	* Initializes the Rooks on the given row for the start of a chess game.
+	*/
+	private ArrayList<Rook> initializeRooksOnRow(int row){
+		ArrayList<Rook> piecesInitialized = new ArrayList<>();
+		//TODO:
+		return piecesInitialized;
+	}	
+	
+	/**
+	* Initializes the Horses on the given row for the start of a chess game.
+	*/
+	private ArrayList<Horse> initializeHorsesOnRow(int row){
+		ArrayList<Horse> piecesInitialized = new ArrayList<>();
+		//TODO:
+		return piecesInitialized;
+	}	
+	
+	/**
+	* Initializes the Bishops on the given row for the start of a chess game.
+	*/
+	private ArrayList<Bishop> initializeBishopsOnRow(int row){
+		ArrayList<Bishop> piecesInitialized = new ArrayList<>();
+		//TODO:
+		return piecesInitialized;
+	}	
+	
+	/**
+	* Initializes the Queen on the given row for the start of a chess game.
+	*/
+	private ArrayList<Queen> initializeQueenOnRow(int row){
+		ArrayList<Queen> piecesInitialized = new ArrayList<>();
+		//TODO:
+		return piecesInitialized;
+	}	
+	
+	/**
+	* Initializes the King on the given row for the start of a chess game.
+	*/
+	private ArrayList<King> initializeKingOnRow(int row){
+		ArrayList<King> piecesInitialized = new ArrayList<>();
+		//TODO:
+		return piecesInitialized;
+	}		
 }
---
+++ model/chess/Horse.java
@@ -0,0 +1,9 @@
+package model.chess;
+
+import java.awt.Point;
+
+public class Horse extends GamePiece{
+	public Horse(Point coord) {
+		super(coord);
+	}
+}
